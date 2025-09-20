44c28e0382ebaa8d4e4ac760b5e4d33775d17eb3
Zsolt Kovacs <zs.myth@gmail.com>
2025-09-20 17:55:06 +0200

    forkEndMerge.puml is converted to pflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,9 @@ dependencies {
   // Logging dependencies
   implementation("org.slf4j:slf4j-api:${slf4jVersion}")
   implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+
   implementation("org.apache.commons:commons-lang3:3.18.0")
+  implementation("org.apache.commons:commons-text:1.14.0")
 
   testImplementation(platform("org.junit:junit-bom:$junitVersion"))
   testImplementation("org.junit.jupiter:junit-jupiter")
--- src/main/groovy/eu/describeit/plantflow/engine/PlantUmlConverter.groovy
+++ src/main/groovy/eu/describeit/plantflow/engine/PlantUmlConverter.groovy
@@ -2,13 +2,15 @@ package eu.describeit.plantflow.engine
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-import org.apache.commons.lang3.StringUtils
+
+import static org.apache.commons.lang3.StringUtils.substringBetween
+import static org.apache.commons.text.CaseUtils.toCamelCase
 
 
 @CompileStatic
 @Slf4j
 final class PlantUmlConverter {
-  static def linesToMethod = ['start', 'stop', 'end', 'fork', 'forkAgain', 'endMerge']
+  static def linesToMethod = ['start', 'stop', 'end', 'fork', 'fork again', 'end merge', 'endif']
 
   static String getResourceText(String file) {
     return PlantUmlConverter.class.getClassLoader().getResource(file).text.trim()
@@ -18,11 +20,13 @@ final class PlantUmlConverter {
     def pflow = new StringBuffer();
 
     pumlText.eachLine { String line ->
-      switch (line) {
+      int tabSize = line.takeWhile { it == ' ' }.size()
+
+      switch (line.substring(tabSize)) {
         case ~/^@.*/       : log.info('convertToPlantFlowDsl() - DROPPING line:{}', line); break
-        case linesToMethod : pflow.append(convertLineToMethod(line)); break
-        case ~/.*:.*;$/    : pflow.append(convertLineToActionMethod(line)); break
-        case 'fork again'  : pflow.append(convertLineToForkAgainMethod(line)); break
+        case ''            : pflow.append(tab(tabSize)).append('\n'); break
+        case linesToMethod : pflow.append(convertLineToMethod(tabSize, line)); break
+        case ~/^ *:.*;$/   : pflow.append(convertLineToActionMethod(tabSize, line)); break
         default            : log.error('convertToPlantFlowDsl() - ???? line:{}', line); break
       }
     }
@@ -30,18 +34,17 @@ final class PlantUmlConverter {
     return pflow.toString()
   }
 
-  private static String convertLineToMethod(String line) {
-    log.info('convertLineToMethod() - line:{}', line)
-    return "${line}()\n"
+  private static String convertLineToMethod(int tabSize, String line) {
+    log.info('convertLineToMethod() - line:{} tabSize:{}', line, tabSize)
+    return tab(tabSize) + toCamelCase(line, false, ' ' as char) + "()\n"
   }
 
-  private static String convertLineToActionMethod(String line) {
-    log.info('convertLineToActionMethod() - line:{}', line)
-    return 'action("' + StringUtils.substringBetween(line, ':', ';') + '")\n'
+  private static String convertLineToActionMethod(int tabSize, String line) {
+    log.info('convertLineToActionMethod() - line:{} tabSize:{}', line, tabSize)
+    return tab(tabSize) + 'action("' + substringBetween(line, ':', ';') + '")\n'
   }
 
-  private static convertLineToForkAgainMethod(String line) {
-    log.info('convertLineToForkAgainMethod() - line:{}', line)
-    return 'forkAgain()'
+  private static String tab(int size) {
+    return ' '.repeat(size)
   }
 }
