624f3681b59df2afb74a849232f75cc137262523
Zsolt Kovacs <zs.myth@gmail.com>
2025-09-21 13:52:24 +0200

    enum to contain all the regex cases

--- src/main/groovy/eu/describeit/plantflow/engine/PlantUmlConverter.groovy
+++ src/main/groovy/eu/describeit/plantflow/engine/PlantUmlConverter.groovy
@@ -3,15 +3,38 @@ package eu.describeit.plantflow.engine
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 
+import java.util.regex.Pattern
+
 import static org.apache.commons.lang3.StringUtils.substringBetween
 import static org.apache.commons.lang3.StringUtils.substringsBetween
 import static org.apache.commons.text.CaseUtils.toCamelCase
-
 
 @CompileStatic
 @Slf4j
 final class PlantUmlConverter {
   static def linesToMethod = ['start', 'stop', 'end', 'fork', 'fork again', 'end merge', 'endif', 'repeat', 'endswitch']
+
+  private static enum ExpressionCase {
+    IF      (~ /^if.*/       , ["iff", 'then']),
+    REPEAT  (~ /^repeat.*/   , ["repeatWhile", 'is', 'not']),
+    WHILE   (~ /^while.*/    , ["whilee", 'is']),
+    ENDWHILE(~ /^endwhile.*/ , ["endwhile"]),
+    ELSE    (~ /^else.*/     , ["elsee"]),
+    SWITCH  (~ /^switch.*/   , ["switchh"]),
+    CASE    (~ /^case.*/     , ["casee"])
+
+    final Pattern matcher
+    final List<String> exprPieces
+
+    ExpressionCase(Pattern pattern, List<String> exprPieces) {
+      this.matcher = pattern
+      this.exprPieces = exprPieces
+    }
+
+    static ExpressionCase match(String line) {
+      values().find { ExpressionCase ec -> (line ==~ ec.matcher) } as ExpressionCase
+    }
+  }
 
   static String getResourceText(String file) {
     return PlantUmlConverter.class.getClassLoader().getResource(file).text.trim()
@@ -25,21 +48,18 @@ final class PlantUmlConverter {
       String lineTrimmed = line.trim()
 
       switch (lineTrimmed) {
-        case ~/^@.*/       : log.info('convertToPlantFlowDsl() - DROPPING line:{}', lineTrimmed); break
+        case ~/^@.*/       : log.debug('convertToPlantFlowDsl() - DROPPING line:{}', lineTrimmed); break
         case ''            : pflow.append(tab(tabSize)); break
         case linesToMethod : pflow.append(convertLineToMethod(tabSize, lineTrimmed)); break
         case ~/^:.*;$/     : pflow.append(convertLineToActionMethod(tabSize, lineTrimmed)); break
-        case ~/^if.*/      : pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, "iff", 'then')); break
-        case ~/^repeat.*/  : pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, "repeatWhile", 'is', 'not')); break
-        case ~/^while.*/   : pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, "whilee", 'is')); break
-        case ~/^endwhile.*/: pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, "endwhile")); break
-        case ~/^else.*/    : pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, "elsee")); break
-        case ~/^switch.*/  : pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, "switchh")); break
-        case ~/^case.*/    : pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, "casee")); break
+
         default :
-          // throw error
-          log.error('convertToPlantFlowDsl() - ???? line:{}', lineTrimmed)
-          throw new IllegalArgumentException()
+          def exprCase = ExpressionCase.match(lineTrimmed)
+          if (exprCase) {
+            pflow.append(convertLineWithExpressions(tabSize, lineTrimmed, exprCase.exprPieces))
+          } else {
+            throw new IllegalArgumentException('Uncovered case for line:' + lineTrimmed )
+          }
           break
       }
       pflow.append('\n')
@@ -66,13 +86,12 @@ final class PlantUmlConverter {
     return tab(tabSize) + 'action("' + actionName + '")'
   }
 
-  private static String convertLineWithExpressions(int tabSize, String line, String... exprPieces) {
+  private static String convertLineWithExpressions(int tabSize, String line, List<String> exprPieces) {
     log.info('convertLineWithExpressions() - line:"{}" , tabSize:{} , exprPieces:{}', line, tabSize, exprPieces)
 
     List<String> exprData = substringsBetween(line, '(', ')').collect { it.trim() }
 
-    assert exprPieces
-    assert exprPieces.length == exprData.size()
+    assert exprPieces.size() == exprData.size()
 
     StringBuilder newLine = new StringBuilder()
 
