ebb1cd4270dfdaafb5437cbbc8af336bcb4d640a
Zsolt Kovacs <zs.myth@gmail.com>
2025-09-21 10:44:23 +0200

    ifThenElseEndif.puml is converted to pflow

--- src/main/groovy/eu/describeit/plantflow/engine/PlantUmlConverter.groovy
+++ src/main/groovy/eu/describeit/plantflow/engine/PlantUmlConverter.groovy
@@ -4,13 +4,14 @@ import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 
 import static org.apache.commons.lang3.StringUtils.substringBetween
+import static org.apache.commons.lang3.StringUtils.substringsBetween
 import static org.apache.commons.text.CaseUtils.toCamelCase
 
 
 @CompileStatic
 @Slf4j
 final class PlantUmlConverter {
-  static def linesToMethod = ['start', 'stop', 'end', 'fork', 'fork again', 'end merge', 'endif']
+  static def linesToMethod = ['start', 'stop', 'end', 'fork', 'fork again', 'end merge', 'endif', 'repeat']
 
   static String getResourceText(String file) {
     return PlantUmlConverter.class.getClassLoader().getResource(file).text.trim()
@@ -21,30 +22,56 @@ final class PlantUmlConverter {
 
     pumlText.eachLine { String line ->
       int tabSize = line.takeWhile { it == ' ' }.size()
+      String lineTrimmed = line.trim()
 
-      switch (line.substring(tabSize)) {
-        case ~/^@.*/       : log.info('convertToPlantFlowDsl() - DROPPING line:{}', line); break
-        case ''            : pflow.append(tab(tabSize)).append('\n'); break
-        case linesToMethod : pflow.append(convertLineToMethod(tabSize, line)); break
-        case ~/^ *:.*;$/   : pflow.append(convertLineToActionMethod(tabSize, line)); break
-        default            : log.error('convertToPlantFlowDsl() - ???? line:{}', line); break
+      switch (lineTrimmed) {
+        case ~/^@.*/       : log.info('convertToPlantFlowDsl() - DROPPING line:{}', lineTrimmed); break
+        case ''            : pflow.append(tab(tabSize)); break
+        case linesToMethod : pflow.append(convertLineToMethod(tabSize, lineTrimmed)); break
+        case ~/^:.*;$/     : pflow.append(convertLineToActionMethod(tabSize, lineTrimmed)); break
+        case ~/^if.*/      : pflow.append(convertLineIfThen(tabSize, lineTrimmed)); break
+        case ~/^else.*/    : pflow.append(convertLineElse(tabSize, lineTrimmed)); break
+        default :
+          // throw error
+          log.error('convertToPlantFlowDsl() - ???? line:{}', lineTrimmed)
+          break
       }
+      pflow.append('\n')
     }
 
     return pflow.toString()
   }
 
+  private static String tab(int size) {
+    return ' '.repeat(size)
+  }
+
   private static String convertLineToMethod(int tabSize, String line) {
-    log.info('convertLineToMethod() - line:{} tabSize:{}', line, tabSize)
-    return tab(tabSize) + toCamelCase(line, false, ' ' as char) + "()\n"
+    log.info('convertLineToMethod() - line:"{}" , tabSize:{}', line, tabSize)
+    return tab(tabSize) + toCamelCase(line, false, ' ' as char) + "()"
   }
 
   private static String convertLineToActionMethod(int tabSize, String line) {
-    log.info('convertLineToActionMethod() - line:{} tabSize:{}', line, tabSize)
-    return tab(tabSize) + 'action("' + substringBetween(line, ':', ';') + '")\n'
+    log.info('convertLineToActionMethod() - line:"{}" , tabSize:{}', line, tabSize)
+    String actionName = substringBetween(line, ':', ';')
+    return tab(tabSize) + 'action("' + actionName + '")'
   }
 
-  private static String tab(int size) {
-    return ' '.repeat(size)
+  private static String convertLineIfThen(int tabSize, String line) {
+    log.info('convertLineIfThen() - line:"{}" , tabSize:{}', line, tabSize)
+
+    def exprData = substringsBetween(line, '(', ')').collect {it.trim()}
+    String newLine = "iff (\"${exprData[0]}\") then (\"${exprData[1]}\")"
+
+    return tab(tabSize) + newLine
+  }
+
+  private static String convertLineElse(int tabSize, String line) {
+    log.info('convertLineElse() - line:"{}" , tabSize:{}', line, tabSize)
+
+    def exprValue = substringBetween(line, '(', ')').trim()
+    String newLine = "elsee (\"${exprValue}\")"
+
+    return tab(tabSize) + newLine
   }
 }
