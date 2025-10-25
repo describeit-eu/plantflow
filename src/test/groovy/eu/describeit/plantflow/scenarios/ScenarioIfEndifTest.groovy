1bed7c257ddcba33b1c482e98f8d9006d64b1ace
Zsolt Kovacs <zs.myth@gmail.com>
2025-10-25 21:41:51 +0200

    split test cases

--- src/test/groovy/eu/describeit/plantflow/scenarios/ScenarioIfEndifTest.groovy
+++ src/test/groovy/eu/describeit/plantflow/scenarios/ScenarioIfEndifTest.groovy
@@ -3,33 +3,11 @@ package eu.describeit.plantflow.scenarios
 import eu.describeit.plantflow.PlantFlow
 import eu.describeit.plantflow.engine.PlantFlowAction
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class ScenarioIfEndifTest extends Specification {
-
-  void 'if Then Else Endif'() {
-    given:
-    PlantFlowAction processAll = Mock() { getName() >> 'process all' }
-    PlantFlowAction processNone = Mock() { getName() >> 'process none' }
-    PlantFlow pflow = new PlantFlow("ifThenElseEndif", [processAll, processNone])
-
-    when:
-    def nextActions = pflow.calculateNext()
-
-    then:
-    1 * processAll.activate() >> true
-    0 * processNone.activate()
-    nextActions[0].name == 'process all'
-
-    when:
-    nextActions = pflow.calculateNext()
-
-    then:
-    1 * processAll.activate() >> false
-    0 * processNone.activate()
-    nextActions.isEmpty()
-  }
-
-  void 'if Then Endif'() {
+  @Unroll
+  void 'basic scenario for pflow: #fileName'() {
     given:
     PlantFlowAction action1 = Mock() { getName() >> 'process all' }
     def pflow = new PlantFlow(fileName, [action1])
---
+++ src/test/groovy/eu/describeit/plantflow/scenarios/ScenarioIfThenElseEndifTest.groovy
@@ -0,0 +1,31 @@
+package eu.describeit.plantflow.scenarios
+
+import eu.describeit.plantflow.PlantFlow
+import eu.describeit.plantflow.engine.PlantFlowAction
+import spock.lang.Specification
+
+class ScenarioIfThenElseEndifTest extends Specification {
+
+  void 'basic scenario'() {
+    given:
+    PlantFlowAction processAll = Mock() { getName() >> 'process all' }
+    PlantFlowAction processNone = Mock() { getName() >> 'process none' }
+    PlantFlow pflow = new PlantFlow("ifThenElseEndif", [processAll, processNone])
+
+    when:
+    def nextActions = pflow.calculateNext()
+
+    then:
+    1 * processAll.activate() >> true
+    0 * processNone.activate()
+    nextActions[0].name == 'process all'
+
+    when:
+    nextActions = pflow.calculateNext()
+
+    then:
+    1 * processAll.activate() >> false
+    0 * processNone.activate()
+    nextActions.isEmpty()
+  }
+}
