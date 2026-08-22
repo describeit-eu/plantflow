b2fb3af3b2556ed18e137d642ea8fd515f9c007c
Zsolt Kovacs <zs.myth@gmail.com>
2026-08-22 15:24:32 +0200

    update dependencies to latest and relocate test resources for better organization

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,15 @@ plugins {
   id("groovy")
 }
 
-val groovyVersion    = "4.0.33"
-val slf4jVersion     = "2.0.17"
-val logbackVersion   = "1.5.18"
+val groovyVersion    = "5.1.0"
+val slf4jVersion     = "2.0.18"
+val logbackVersion   = "1.6.3"
 
-val junitVersion     = "5.13.4"
-val spockVersion     = "2.4-groovy-4.0"
-//val bytebuddyVersion = "1.17.7"
-//val mockitoVersion   = "5.20.0"
-val jsonUnitVersion  = "5.0.0"
+val junitVersion     = "6.1.3"
+val spockVersion     = "2.4-groovy-5.0"
+//val bytebuddyVersion = "1.18.12"
+//val mockitoVersion   = "5.23.0"
+val jsonUnitVersion  = "6.2.0"
 
 group = "eu.describeit"
 version = "1.0-SNAPSHOT"
@@ -23,15 +23,15 @@ dependencies {
   implementation("org.apache.groovy:groovy:$groovyVersion")
   implementation("org.apache.groovy:groovy-json:$groovyVersion")
   implementation("org.apache.groovy:groovy-templates:$groovyVersion")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.22.2")
 
   // Logging dependencies
   implementation("org.slf4j:slf4j-api:${slf4jVersion}")
   implementation("ch.qos.logback:logback-classic:${logbackVersion}")
 
   // Commons dependencies
-  implementation("org.apache.commons:commons-lang3:3.18.0")
-  implementation("org.apache.commons:commons-text:1.14.0")
+  implementation("org.apache.commons:commons-lang3:3.20.0")
+  implementation("org.apache.commons:commons-text:1.15.0")
     
   // Test dependencies
   testImplementation(platform("org.junit:junit-bom:$junitVersion"))
